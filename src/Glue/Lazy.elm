979f667b8342647b6d7f1d9bc196c161a926ab3d
Marek Fajkus <marek.faj@gmail.com>
2019-06-04 18:36:42 +0200

    spike documentation

--- src/Glue/Lazy.elm
+++ src/Glue/Lazy.elm
@@ -1,29 +1,90 @@
-module Glue.Lazy exposing (LazyGlue)
+module Glue.Lazy exposing
+    ( LazyGlue
+    , initLater, forceInit, forceInitModel, ensure, ensureModel
+    , update, updateModel, updateWith, updateModelWith, trigger
+    , subscriptions, subscriptionsWhen
+    , view
+    )
+
+{-| In some case it makes sense to postpone initialization
+of some module after some action like for instance a router change.
+
+Such part of state can expressed using `Maybe` type like
+
+    type alias Model
+        { about : About.Model
+        , userSettings : Maybe UserSettings.Model
+        }
+
+or for instance using sum type like
+
+    type Model
+        = About About.Model
+        | UserSettings UserSettings.Model
+
+In such case though programmer is responsible for manually initializing
+the module at the right time.
+
+@docs LazyGlue
+
+
+## Initialization
+
+@docs initLater, forceInit, forceInitModel, ensure, ensureModel
+
+
+## Updates
+
+@docs update, updateModel, updateWith, updateModelWith, trigger
+
+
+## Subscriptions
+
+@docs subscriptions, subscriptionsWhen
+
+
+## View
+
+@docs view
+
+-}
 
 import Glue exposing (Glue)
 import Glue.Internal
 import Html exposing (Html)
 
 
+{-| -}
 type alias LazyGlue model subModel msg subMsg =
     Glue.Internal.Glue model (Maybe subModel) msg subMsg
 
 
+{-| Compatible with Glue.init style initialization but assigns `Nothing` to the part of model that
+should be initalized later. This function is useful in cases using record with maybes.
+-}
 initLater : Glue model subModel msg subMsg -> ( Maybe subModel -> a, Cmd msg ) -> ( a, Cmd msg )
 initLater _ ( f, cmd ) =
     ( f Nothing, cmd )
 
 
+{-| Force intialization of module. If model state already exists in the form of `Just model`
+this value is overwrite nontherless
+-}
 forceInit : LazyGlue model subModel msg subMsg -> ( subModel, Cmd subMsg ) -> ( model, Cmd msg ) -> ( model, Cmd msg )
 forceInit glue pair =
     Glue.updateWith glue (\_ -> Tuple.mapFirst Just pair)
 
 
+{-| Force intialization of module. If model state already exists in the form of `Just model`
+this value is overwrite nontherless
+-}
 forceInitModel : LazyGlue model subModel msg subMsg -> subModel -> model -> model
 forceInitModel glue val =
     Glue.updateModelWith glue (\_ -> Just val)
 
 
+{-| Initialize model only when state doesn't already exist (`Nothing` is current value)
+-}
 ensure : LazyGlue model subModel msg subMsg -> (() -> ( subModel, Cmd subMsg )) -> ( model, Cmd msg ) -> ( model, Cmd msg )
 ensure ((Glue.Internal.Glue rec) as glue) f ( model, cmd ) =
     case rec.get model of
@@ -34,6 +95,8 @@ ensure ((Glue.Internal.Glue rec) as glue) f ( model, cmd ) =
             forceInit glue (f ()) ( model, cmd )
 
 
+{-| Initialize model only when state doesn't already exist (`Nothing` is current value)
+-}
 ensureModel : LazyGlue model subModel msg subMsg -> (() -> subModel) -> model -> model
 ensureModel ((Glue.Internal.Glue rec) as glue) f model =
     case rec.get model of
@@ -44,43 +107,82 @@ ensureModel ((Glue.Internal.Glue rec) as glue) f model =
             forceInitModel glue (f ()) model
 
 
+{-| Like `Glue.update` but for `LazyGlue` variant.
+Update is called only when there model is already initialized.
+In cases where update should be forcing intialization use this in
+conjuction with `ensure`[#ensure]
+-}
 update : LazyGlue model subModel msg subMsg -> (a -> subModel -> ( subModel, Cmd subMsg )) -> a -> ( model, Cmd msg ) -> ( model, Cmd msg )
 update glue f =
     Glue.update glue (\a -> patch (f a))
 
 
+{-| Like `Glue.updateModel` but for `LazyGlue` variant.
+Update is called only when there model is already initialized.
+In cases where update should be forcing intialization use this in
+conjuction with `ensureModel`[#ensureModel]
+-}
 updateModel : LazyGlue model subModel msg subMsg -> (a -> subModel -> subModel) -> a -> model -> model
 updateModel glue f =
     Glue.updateModel glue (\a -> Maybe.map (f a))
 
 
+{-| Like `Glue.updateWith` but for `LazyGlue` variant.
+Update is called only when there model is already initialized.
+In cases where update should be forcing intialization use this in
+conjuction with `ensure`[#ensure]
+-}
 updateWith : LazyGlue model subModel msg subMsg -> (subModel -> ( subModel, Cmd subMsg )) -> ( model, Cmd msg ) -> ( model, Cmd msg )
 updateWith glue f =
     Glue.updateWith glue (patch f)
 
 
-updateWithModel : LazyGlue model subModel msg subMsg -> (subModel -> subModel) -> model -> model
-updateWithModel glue f =
+{-| Like `Glue.updateModelWith` but for `LazyGlue` variant.
+Update is called only when there model is already initialized.
+In cases where update should be forcing intialization use this in
+conjuction with `ensureModel`[#ensureModel]
+-}
+updateModelWith : LazyGlue model subModel msg subMsg -> (subModel -> subModel) -> model -> model
+updateModelWith glue f =
     Glue.updateModelWith glue (Maybe.map f)
 
 
-tigger : LazyGlue model subModel msg subMsg -> (subModel -> Cmd subMsg) -> ( model, Cmd msg ) -> ( model, Cmd msg )
-tigger glue fc =
+{-| Like `Glue.triger` but for `LazyGlue` variant.
+Update is called only when there model is already initialized.
+In cases where update should be forcing intialization use this in
+conjuction with `ensure`[#ensure]
+-}
+trigger : LazyGlue model subModel msg subMsg -> (subModel -> Cmd subMsg) -> ( model, Cmd msg ) -> ( model, Cmd msg )
+trigger glue fc =
     Glue.trigger glue (Maybe.withDefault Cmd.none << Maybe.map fc)
 
 
+{-| Like `Glue.subscriptions` but for `LazyGlue` variant.
+Update is called only when there model is already initialized.
+In cases where update should be forcing intialization use this in
+conjuction with `ensure`[#ensure]
+-}
 subscriptions : LazyGlue model subModel msg subMsg -> (subModel -> Sub subMsg) -> (model -> Sub msg) -> (model -> Sub msg)
 subscriptions glue f =
     (\m -> Maybe.withDefault Sub.none (Maybe.map f m))
         |> Glue.subscriptions glue
 
 
+{-| Like `Glue.subscriptionsWhen` but for `LazyGlue` variant.
+Update is called only when there model is already initialized.
+In cases where update should be forcing intialization use this in
+conjuction with `ensure`[#ensure]
+-}
 subscriptionsWhen : (model -> Bool) -> LazyGlue model subModel msg subMsg -> (subModel -> Sub subMsg) -> (model -> Sub msg) -> (model -> Sub msg)
 subscriptionsWhen predicate glue f =
     (\m -> Maybe.withDefault Sub.none (Maybe.map f m))
         |> Glue.subscriptionsWhen predicate glue
 
 
+{-| Similar to `Glue.view` but forces user to handle `Nothing` case
+because API of this module can't really guarantee view won't be called
+with uninitialized Model.
+-}
 view : LazyGlue model subModel msg subMsg -> (subModel -> Html subMsg) -> model -> Maybe (Html msg)
 view (Glue.Internal.Glue rec) v model =
     Maybe.map (Html.map rec.msg << v) <| rec.get model
